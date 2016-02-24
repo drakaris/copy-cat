a185e428a42c8338f4bfd3ac2ccc8b793007e179
Arjhun Srinivas <arjhun.s@gmail.com>
2016-02-24 22:08:10 +0530

    Updated deep scraper

--- deep_scraper.py
+++ deep_scraper.py
@@ -14,6 +14,19 @@ def populate_menu(item,xpath):
 	for e in elements:
 		scrape_list[item].append(e.get_attribute('href'))
 
+def scrape_clothing(url):
+	global browser
+
+	# Load page
+	browser.get(url)
+
+	# Toggle all products - off
+	browser.find_element_by_xpath('//*[@id="js-filtered_items"]/div[1]/div/div[2]/div[1]/div/ul/li[2]/a').click()
+
+	filters = browser.find_elements_by_xpath('//*[@id="js-filters"]//span')
+	for f in filters:
+		print f.text
+
 # Scrape structure
 scrape_list = {
 	'clothing' : [],
@@ -35,6 +48,4 @@ populate_menu('clothing','//*[@id="js-header__nav"]/li[2]/ul/li/a')
 # Populate menu list for accessories
 populate_menu('accessories','//*[@id="js-header__nav"]/li[6]/ul/li/a')
 
-print scrape_list
-
-browser.quit()
+scrape_clothing(scrape_list['clothing'][0])
